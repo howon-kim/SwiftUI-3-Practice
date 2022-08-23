5919674a0972b5be168781c4fba53eec0b10ca68
Howon Kim <journal-02-permit@icloud.com>
2022-08-22 19:08:02 -0700

    Pinch App Initialize

---
+++ 2.Pinch/Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Howon Kim on 8/22/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
