40a130da7633d1768dcc9b0b82d9b3d5c1a0f2ea
Howon Kim <journal-02-permit@icloud.com>
2022-08-27 17:07:31 -0700

    Initialize Fructus Project

---
+++ 3.Fructus/Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Howon Kim on 8/27/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
