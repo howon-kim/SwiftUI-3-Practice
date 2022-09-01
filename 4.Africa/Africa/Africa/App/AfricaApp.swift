df30138ba09d5266cda42a8ddc282e8e3bdc32cf
Howon Kim <journal-02-permit@icloud.com>
2022-09-01 13:43:23 -0700

    Africa Project initialized

---
+++ 4.Africa/Africa/Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Howon Kim on 9/1/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
