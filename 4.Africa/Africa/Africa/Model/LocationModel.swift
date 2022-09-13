0badd5d1afbf477f879d708ce0bf455c2219efa0
Howon Kim <journal-02-permit@icloud.com>
2022-09-13 13:34:05 -0700

    Map Annotation Updated

---
+++ 4.Africa/Africa/Africa/Model/LocationModel.swift
@@ -0,0 +1,22 @@
+//
+//  LocationModel.swift
+//  Africa
+//
+//  Created by Howon Kim on 9/13/22.
+//
+
+import Foundation
+import MapKit
+
+struct NationalParkLocation: Codable, Identifiable {
+    var id: String
+    var name: String
+    var image: String
+    var latitude: Double
+    var longitude: Double
+    
+    // Computed Property
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+}
