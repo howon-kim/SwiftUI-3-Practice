3725bf7de5ce5a6cd1c32b0fec03a4669b2c30e1
Howon Kim <journal-02-permit@icloud.com>
2022-09-26 19:16:51 -0700

    Finished Devote Project with Widget Completion

--- 6.Devote/DEVOTE/Devote/VIEW/ContentView.swift
+++ 6.Devote/DEVOTE/Devote/VIEW/ContentView.swift
@@ -66,6 +66,8 @@ struct ContentView: View {
                         Button(action: {
                             // TOGGLE APPEARANCE
                             isDarkMode.toggle()
+                            playSound(sound: "sound-tap", type: "mp3")
+                            feedback.notificationOccurred(.success)
                         }, label: {
                             Image(systemName: isDarkMode ? "moon.circle.fill" : "moon.circle")
                                 .resizable()
@@ -81,6 +83,8 @@ struct ContentView: View {
                     // MARK: - NEW TASK BUTTON
                     Button(action: {
                         showNewTaskItem = true
+                        playSound(sound: "sound-ding", type: "mp3")
+                        feedback.notificationOccurred(.success)
                     }, label: {
                         Image(systemName:"plus.circle")
                             .font(.system(size: 30, weight: .semibold, design: .rounded))
@@ -108,9 +112,14 @@ struct ContentView: View {
                     .padding(.vertical, 0)
                     .frame(maxWidth: 640)
                 } //: VSTACK
+                .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
+                .transition(.move(edge: .bottom))
+                .animation(.easeOut(duration: 0.5))
+                
                 // MARK: - NEW TASK ITEM
                 if showNewTaskItem {
-                    BlankView()
+                    BlankView(backgroundColor: isDarkMode ? Color.black : Color.gray,
+                              backgroundOpacity: isDarkMode ? 0.3 : 0.5)
                         .onTapGesture {
                             withAnimation() {
                                 showNewTaskItem = false
@@ -126,6 +135,7 @@ struct ContentView: View {
             .navigationBarHidden(true)
             .background(
                 BackgroundImageView()
+                    .blur(radius: showNewTaskItem ? 8.0 : 0, opaque: false)
             )
             .background(
                 backgroundGradient.ignoresSafeArea(.all)
