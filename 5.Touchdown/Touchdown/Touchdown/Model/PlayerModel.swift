df093e762b7ee7997839fea88a33795b695ece50
Howon Kim <journal-02-permit@icloud.com>
2022-10-02 13:43:58 -0700

    Oct 1

---
+++ 5.Touchdown/Touchdown/Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,14 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Howon Kim on 10/1/22.
+//
+
+import Foundation
+
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
---
+++ 5.Touchdown/Touchdown/Touchdown/View/Home/FeaturedTabView.swift
@@ -0,0 +1,29 @@
+//
+//  FeaturedTabView.swift
+//  Touchdown
+//
+//  Created by Howon Kim on 10/1/22.
+//
+
+import SwiftUI
+
+struct FeaturedTabView: View {
+    var body: some View {
+        TabView {
+            ForEach (players) { player in
+                FeaturedItemView(player: player)
+                    .padding(.top, 10)
+                    .padding(.horizontal, 15)
+                
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+    }
+}
+
+struct FeaturedTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        FeaturedTabView()
+            .background(Color.gray)
+    }
+}
