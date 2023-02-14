09b7bc9ce002b920550700ff6241c24e10620e9b
Howon Kim <journal-02-permit@icloud.com>
2023-02-13 17:47:22 -0800

    021323 Touchdown

--- 5.Touchdown/Touchdown/Touchdown/App/ContentView.swift
+++ 5.Touchdown/Touchdown/Touchdown/App/ContentView.swift
@@ -27,6 +27,20 @@ struct ContentView: View {
                             .padding(.vertical, 20)
                             .frame(height: 300)
                         
+                        CategoryGridView()
+                        
+                        TitleView(title: "Helmets")
+                        
+                        LazyVGrid(columns: gridLayout, spacing: 15, content: {
+                            ForEach(products) { product in
+                                ProductItemView(product: product)
+                            } //: LOOP
+                        }) //: GRID
+                        .padding(15)
+                        
+                        TitleView(title: "Brands")
+                        BrandGridView()
+                        
                         FooterView()
                             .padding(.horizontal)
                     } //: VSTACK
---
+++ 5.Touchdown/Touchdown/Touchdown/Model/CategoryModel.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryModel.swift
+//  Touchdown
+//
+//  Created by Howon Kim on 2/13/23.
+//
+
+import Foundation
+
+struct Category: Codable, Identifiable {
+    
+    let id: Int
+    let name: String
+    let image: String
+    
+}
+
