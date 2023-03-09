50b9af12a3af6fe7decce41b46b21b19abbc4060
Howon Kim <journal-02-permit@icloud.com>
2023-03-08 19:42:19 -0800

    TouchDown Project Completed

---
+++ 5.Touchdown/Touchdown/Touchdown/View/Detail/HeaderDetailView.swift
@@ -0,0 +1,33 @@
+//
+//  HeaderDetailView.swift
+//  Touchdown
+//
+//  Created by Howon Kim on 3/8/23.
+//
+
+import SwiftUI
+
+struct HeaderDetailView: View {
+    @EnvironmentObject var shop: Shop
+    var body: some View {
+        VStack(alignment: .leading, spacing: 6, content: {
+            Text("Protective Gear")
+            
+            Text(shop.selectedProduct?.name ?? sampleProduct.name)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+        })
+        .foregroundColor(.white)
+
+    }
+}
+
+struct HeaderDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderDetailView()
+            .environmentObject(Shop())
+            .previewLayout(.sizeThatFits)
+            .padding()
+            .background(.gray)
+    }
+}
